86804e0563aa50acc1777c275959b23c83b85858
Jim Dixon <jddixon@gmail.com>
2016-12-14 16:40:01 -0800

    rearranging things for wireops split

---
+++ zoggery_proto_spec.py
@@ -0,0 +1,13 @@
+# ~/dev/py/fieldz/zoggeryProtoSpec.py
+
+ZOGGERY_PROTO_SPEC = """
+protocol org.xlattice.zoggery
+
+message logEntry:
+ timestamp  fuint32
+ nodeID     fbytes20
+ key        fbytes20
+ length     vuint32
+ by         lstring
+ path       lstring
+"""
